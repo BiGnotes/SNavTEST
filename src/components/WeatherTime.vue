28c05bcf42e5f78efc7aa455303593d6f12ddd3d
BiGnotes <74706496+BiGnotes@users.noreply.github.com>
2025-08-06 18:12:16 +0800

    Update WeatherTime.vue

--- src/components/WeatherTime.vue
+++ src/components/WeatherTime.vue
@@ -43,7 +43,7 @@
       <span class="weekday">{{ timeData.weekday ?? "星期八" }}</span>
     </div>
     <div v-if="set.showWeather" class="weather">
-      <span class="city">{{ adCodeResult.result.ad_info?.city ?? "N/A" }}</span>
+      <span class="city">{{ weatherData?.city ?? "N/A" }}</span>
       <span class="status">{{ weatherData?.condition ?? "N/A" }}</span>
       <span class="temperature">{{ weatherData?.temp ?? "N/A" }} ℃</span>
       <span class="wind">{{ weatherData?.windDir ?? "N/A" }}</span>
@@ -102,6 +102,7 @@ const getWeatherData = async () => {
     }
     const data = weatherResult.lives[0];
     weatherData.value = {
+      city: data.city,
       condition: data.weather,
       temp: data.temperature,
       windDir: data.winddirection + "风",
